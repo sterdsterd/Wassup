f81f9d4a2ae9d5a0ee70bc2e78d0c27498107ce4
sterdsterd <sterdsterd@icloud.com>
2019-08-20 17:00:27 +0900

    Update Gradle Version

--- app/src/main/java/net/sterdsterd/wassup/activity/AddActivity.kt
+++ app/src/main/java/net/sterdsterd/wassup/activity/AddActivity.kt
@@ -12,12 +12,6 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
