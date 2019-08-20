14e4a238ba3451cbfec4bf682dc09fde27914d61
sterdsterd <sterdsterd@icloud.com>
2019-08-20 11:18:22 +0900

    Added CollapsingToolbar in InfoActivity

--- app/src/main/java/net/sterdsterd/wassup/activity/InfoActivity.kt
+++ app/src/main/java/net/sterdsterd/wassup/activity/InfoActivity.kt
@@ -13,11 +13,11 @@ import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
 import android.widget.ArrayAdapter
+import androidx.core.content.res.ResourcesCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.storage.FirebaseStorage
 import io.github.pierry.progress.Progress
-import kotlinx.android.synthetic.main.activity_edit.*
 import kotlinx.android.synthetic.main.activity_info.profile
 import net.sterdsterd.wassup.SharedData
 import java.io.File
@@ -29,7 +29,6 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
-
         val progress = Progress(this)
         progress.setBackgroundColor(Color.parseColor("#323445"))
             .setMessage("Loading")
@@ -38,14 +37,15 @@ class InfoActivity : AppCompatActivity() {
         val id = intent.extras.getString("id")
         val data = SharedData.studentList.filter { it0 -> it0.id == id }[0]
 
-        tvTitle.text = data.name
+        collapsingToolBar.title = data.name
+        collapsingToolBar.setCollapsedTitleTypeface(ResourcesCompat.getFont(this, R.font.spoqa_bold))
+        collapsingToolBar.setExpandedTitleTypeface(ResourcesCompat.getFont(this, R.font.spoqa_bold))
 
         val file = File(cacheDir.toString()).listFiles().filter { it.name == "$id${data.hash}.jpg" }[0]
         Glide.with(this)
             .asBitmap()
             .load(file)
             .error(R.drawable.ic_profile)
-            .apply(RequestOptions.circleCropTransform())
             .into(profile)
 
         val firestore = FirebaseFirestore.getInstance()
@@ -54,9 +54,8 @@ class InfoActivity : AppCompatActivity() {
         val classStr = pref.getString("class", "Null")
         firestore.collection("class").document(classStr).collection("memberList").document(id).get().addOnCompleteListener { t ->
             if(t.isComplete) {
-                tvName.setText(t.result?.getString("name"))
-                tvStuPhone.setText(PhoneNumberUtils.formatNumber(t.result?.getString("studentPhone"), Locale.getDefault().getCountry()))
-                tvParentPhone.setText(PhoneNumberUtils.formatNumber(t.result?.getString("parentPhone"), Locale.getDefault().getCountry()))
+                tvStuPhone.setText(PhoneNumberUtils.formatNumber(t.result?.getString("studentPhone"), Locale.getDefault().country))
+                tvParentPhone.setText(PhoneNumberUtils.formatNumber(t.result?.getString("parentPhone"), Locale.getDefault().country))
 
                 callStudent.setOnClickListener{
                     startActivity(Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", t.result?.getString("studentPhone"), null)))
@@ -70,7 +69,7 @@ class InfoActivity : AppCompatActivity() {
 
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mutableListOf("출석", "결석", "탑승"))
 
-        back.setOnClickListener {
+        btnToolbar.setOnClickListener {
             super.finish()
         }
 
--- app/src/main/java/net/sterdsterd/wassup/adapter/EditAdapter.kt
+++ app/src/main/java/net/sterdsterd/wassup/adapter/EditAdapter.kt
@@ -2,6 +2,7 @@ package net.sterdsterd.wassup.adapter
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.telephony.PhoneNumberUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -17,6 +18,7 @@ import net.sterdsterd.wassup.MemberData
 import net.sterdsterd.wassup.R
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.util.*
 
 
 class EditAdapter(val activity: MainActivity, val items : MutableList<MemberData>) : RecyclerView.Adapter<EditAdapter.MainViewHolder>() {
@@ -32,7 +34,7 @@ class EditAdapter(val activity: MainActivity, val items : MutableList<MemberData
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         holder.name.text = items[position].name
-        holder.phone.text = items[position].phone
+        holder.phone.text = PhoneNumberUtils.formatNumber(items[position].phone, Locale.getDefault().country)
         holder.item.setOnClickListener { v ->
             val intent = Intent(v.context, EditActivity::class.java)
             intent.putExtra("id", items[position].id)
