9a77954613ee555761f105dabd1e71d2328dc68b
sterdsterd <sterdsterd@icloud.com>
2019-09-09 16:31:43 +0900

    Added Logout Feature

--- app/src/main/java/net/sterdsterd/wassup/fragment/InfoFragment.kt
+++ app/src/main/java/net/sterdsterd/wassup/fragment/InfoFragment.kt
@@ -2,6 +2,7 @@ package net.sterdsterd.wassup.fragment
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
+import com.marcoscg.dialogsheet.DialogSheet
 import kotlinx.android.synthetic.main.fragment_edit.*
 import kotlinx.android.synthetic.main.fragment_info.*
 import net.sterdsterd.wassup.activity.MainActivity
@@ -16,6 +18,7 @@ import net.sterdsterd.wassup.adapter.EditAdapter
 import net.sterdsterd.wassup.R
 import net.sterdsterd.wassup.SharedData
 import net.sterdsterd.wassup.activity.EditActivity
+import net.sterdsterd.wassup.activity.SplashActivity
 
 
 class InfoFragment : Fragment() {
@@ -42,6 +45,12 @@ class InfoFragment : Fragment() {
         tvRole.text = if (pref!!.getString("role", "Null") == "class") "담임 교사" else "지도 교사"
         tvClass.text = classStr
 
+        logout.setOnClickListener {
+            val pref = activity?.getSharedPreferences("User", Context.MODE_PRIVATE)
+            pref?.edit()?.clear()?.commit()
+            startActivity(Intent(activity, SplashActivity::class.java))
+            activity?.finish()
+        }
     }
 
 }
