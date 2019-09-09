d50a9468ff4e9511ad0d3e6728119515f5d929d2
sterdsterd <sterdsterd@icloud.com>
2019-09-10 02:06:05 +0900

    Apply Custom DatePicker Style

--- app/src/main/java/net/sterdsterd/wassup/activity/MainActivity.kt
+++ app/src/main/java/net/sterdsterd/wassup/activity/MainActivity.kt
@@ -93,7 +93,7 @@ class MainActivity : AppCompatActivity() {
 
                     if (Build.VERSION.SDK_INT >= 24) {
                         val cal = Calendar.getInstance(TimeZone.getDefault())
-                        DatePickerDialog(this, listener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+                        DatePickerDialog(this, R.style.DialogTheme, listener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
                     }
 
                 }
