24359a901ba6de783e149e22d8fd54dfdddee3a1
sterdsterd <sterdsterd@icloud.com>
2019-09-11 14:52:41 +0900

    Added Password Checking Mechanism

--- app/src/main/java/net/sterdsterd/wassup/activity/MyInfoActivity.kt
+++ app/src/main/java/net/sterdsterd/wassup/activity/MyInfoActivity.kt
@@ -1,6 +1,7 @@
 package net.sterdsterd.wassup.activity
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableStringBuilder
@@ -10,10 +11,14 @@ import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import at.favre.lib.crypto.bcrypt.BCrypt
+import com.google.firebase.firestore.FirebaseFirestore
 import com.marcoscg.dialogsheet.DialogSheet
 import net.sterdsterd.wassup.R
 
 import kotlinx.android.synthetic.main.activity_my_info.*
+import kotlinx.android.synthetic.main.activity_my_info.etId
+import kotlinx.android.synthetic.main.activity_my_info.etPwd
 
 class MyInfoActivity : AppCompatActivity() {
 
@@ -49,9 +54,48 @@ class MyInfoActivity : AppCompatActivity() {
 
             val etPwd = confirmation.inflatedView.findViewById<EditText>(R.id.etPwd)
 
+            val progress: DialogSheet = DialogSheet(this@MyInfoActivity)
+                .setColoredNavigationBar(true)
+                .setCancelable(true)
+                .setRoundedCorners(true)
+                .setBackgroundColor(Color.parseColor("#323445"))
+                .setView(R.layout.bottom_sheet_progress)
+
             confirmation.setPositiveButton("확인") {
-                //TODO : 비밀번호 체크 후 정보 변경
-                Toast.makeText(this, etPwd.text, Toast.LENGTH_LONG).show()
+                progress.show()
+                val firestore = FirebaseFirestore.getInstance()
+                firestore.collection("member").document(etId.text.toString()).get().addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        if (BCrypt.verifyer().verify(etPwd.text.toString().toCharArray(), it.result?.data?.get("pwd").toString()).verified) {
+                            //TODO : 정보 변경
+                            val correct: DialogSheet = DialogSheet(this@MyInfoActivity)
+                                .setColoredNavigationBar(true)
+                                .setCancelable(true)
+                                .setRoundedCorners(true)
+                                .setTitle("변경했어요")
+                                .setMessage("ㅇㅇ")
+                                .setPositiveButton("닫기") {
+                                    finish()
+                                }
+                                .setBackgroundColor(Color.parseColor("#323445"))
+                            correct.show()
+                        } else {
+                            val wrong: DialogSheet = DialogSheet(this@MyInfoActivity)
+                                .setColoredNavigationBar(true)
+                                .setCancelable(true)
+                                .setRoundedCorners(true)
+                                .setTitle("암호가 틀렸어요")
+                                .setMessage("다시 시도해주세요")
+                                .setPositiveButton("재시도") {
+                                    save.performClick()
+                                }
+                                .setNegativeButton("취소") { }
+                                .setBackgroundColor(Color.parseColor("#323445"))
+                            wrong.show()
+                        }
+                        progress.dismiss()
+                    }
+                }
 
             }
             .setNegativeButton("취소") { }
